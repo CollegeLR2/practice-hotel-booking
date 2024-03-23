ba8c000b63f5a682dbd1365048d57353f0709095
L Rogers <er339467@truro-penwith.ac.uk>
2024-03-23 12:02:45 +0000

    Install tailwind

---
+++ src/pages/index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+
+<?php include "../components/header.php" ?>
+
+<body>
+  <h1 class="text-3xl text-red-600 font-bold underline">
+    Hello world!
+  </h1>
+</body>
+</html>
